0e2e689c2645ea79bfcfcd6d6841acc5bbccc230
sachinkum0009 <sachinkum123567@gmail.com>
2020-04-09 11:36:00 +0530

    square unit test

--- turtle_pkg/test/square.py
+++ turtle_pkg/test/square.py
@@ -7,11 +7,11 @@ import sys
 import unittest
 
 ## A sample python unit test
-class TestBareBones(unittest.TestCase):
+class Square(unittest.TestCase):
     ## test 1 == 1
     def test_one_equals_one(self): # only functions with 'test_'-prefix will be run!
         self.assertEquals(1, 1, "1!=1")
 
 if __name__ == '__main__':
     import rostest
-    rostest.rosrun(PKG, 'test_bare_bones', TestBareBones)
+    rostest.rosrun(PKG, 'square', Square)
