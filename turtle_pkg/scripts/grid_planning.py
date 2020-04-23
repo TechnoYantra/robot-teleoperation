c02359ef32ae877c365ba3b9683a23c3ed6cc2d1
sachinkum0009 <sachinkum123567@gmail.com>
2020-04-23 12:41:02 +0530

    straight line program

--- turtle_pkg/scripts/grid_planning.py
+++ turtle_pkg/scripts/grid_planning.py
@@ -1,7 +1,6 @@
-#! /home/hunter/development/anaconda3/bin/python
+#! /usr/bin/env python
 
 import rospy
-from grid_based_sweep_path_planner import planning
 from sensor_msgs.msg import NavSatFix
 from move_base_msgs.msg import MoveBaseActionResult
 
@@ -11,15 +10,8 @@ class GridPathPlannig:
     def __init__(self):
         rospy.init_node('grid_path_planning')
 
-        lat = 49.9000869191
-        lng = 8.89990548393
-        ox = [lat, lat+0.0000200, lat+0.0000200, lat, lat]
-        oy = [lng, lng, lng+0.0000200, lng+0.0000200, lng]
-        reso = 0.0000025
-
-        self.px, self.py = planning(ox, oy, reso)
-
-        self.i = 0
+        self.lat = 49.9000869191
+        self.lng = 8.89990548393
         move_sub = rospy.Subscriber('/move_base/result',MoveBaseActionResult,self.feedback_cb)
 
         rospy.spin()
@@ -31,21 +23,21 @@ class GridPathPlannig:
     def feedback_cb(self,msg):
         if (msg.status.status == 3):
             rospy.loginfo('result is success')
-            self.publish_next_goal(self.i)
+            self.publish_next_goal()
 
 
 
-    def publish_next_goal(self,i):
+    def publish_next_goal(self):
         gps_pub = rospy.Publisher('/gps_goal_fix',NavSatFix,queue_size=10)
         gps_data = NavSatFix()
         gps_data.header.frame_id='/world'
-        gps_data.latitude=self.px[self.i]
-        rospy.loginfo('latitude is : '+str(self.px[self.i]))
-        gps_data.longitude=self.py[self.i]
-        rospy.loginfo('longitude is : '+str(self.py[self.i]))
+        self.lat=self.lat+0.00000004000
+        gps_data.latitude=self.lat
+        rospy.loginfo('latitude is : '+str(self.lat))
+        gps_data.longitude=self.lng
+        rospy.loginfo('longitude is : '+str(self.lng))
         rospy.loginfo('publishing next goal')
         gps_pub.publish(gps_data)
-        self.i = self.i+1
 
 
 
