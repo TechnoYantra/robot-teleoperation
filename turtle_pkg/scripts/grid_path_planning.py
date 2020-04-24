8eb0c6b3c80545fae62a582034f129f79550a8c7
sachinkum0009 <sachinkum123567@gmail.com>
2020-04-24 10:23:58 +0530

    gps navigation costmap clear

--- turtle_pkg/scripts/grid_path_planning.py
+++ turtle_pkg/scripts/grid_path_planning.py
@@ -4,6 +4,7 @@ import rospy
 from grid_based_sweep_path_planner import planning
 from sensor_msgs.msg import NavSatFix
 from move_base_msgs.msg import MoveBaseActionResult
+from std_srvs.srv import Empty
 
 
 
@@ -20,6 +21,8 @@ class GridPathPlannig:
         self.px, self.py = planning(ox, oy, reso)
 
         self.i = 0
+        rospy.wait_for_service('/move_base/clear_costmaps')
+        self.clear_costmap = rospy.ServiceProxy('/move_base/clear_costmaps',Empty)
         move_sub = rospy.Subscriber('/move_base/result',MoveBaseActionResult,self.feedback_cb)
 
         rospy.spin()
@@ -31,21 +34,44 @@ class GridPathPlannig:
     def feedback_cb(self,msg):
         if (msg.status.status == 3):
             rospy.loginfo('result is success')
+            rospy.logwarn('clearing costmap')
+            ret = self.clear_costmap()
             self.publish_next_goal(self.i)
+        elif (msg.status.status == 2):
+            rospy.loginfo('there is some error so clearing the costmap')
+            ret = self.clear_costmap()
+            self.publish_previous_goal(self.i)
+        elif (msg.status.status == 4):
+            rospy.loginfo('there is some warning so clearing the costmap')
+            ret = self.clear_costmap()
+            self.publish_previous_goal(self.i)
 
-
-
-    def publish_next_goal(self,i):
-        gps_pub = rospy.Publisher('/gps_goal_fix',NavSatFix,queue_size=10)
+    def publish_previous_goal(self,i):
+        gps_pub = rospy.Publisher('/gps_goal_fix', NavSatFix, queue_size=10)
         gps_data = NavSatFix()
         gps_data.header.frame_id='/world'
         gps_data.latitude=self.px[self.i]
         rospy.loginfo('latitude is : '+str(self.px[self.i]))
         gps_data.longitude=self.py[self.i]
         rospy.loginfo('longitude is : '+str(self.py[self.i]))
+        rospy.logerr('number is '+str(self.i))
         rospy.loginfo('publishing next goal')
         gps_pub.publish(gps_data)
+
+
+
+    def publish_next_goal(self,i):
         self.i = self.i+1
+        gps_pub = rospy.Publisher('/gps_goal_fix',NavSatFix,queue_size=10)
+        gps_data = NavSatFix()
+        gps_data.header.frame_id='/world'
+        rospy.logerr('number is '+str(self.i))
+        gps_data.latitude=self.px[self.i]
+        rospy.loginfo('latitude is : '+str(self.px[self.i]))
+        gps_data.longitude=self.py[self.i]
+        rospy.loginfo('longitude is : '+str(self.py[self.i]))
+        rospy.loginfo('publishing next goal')
+        gps_pub.publish(gps_data)
 
 
 
