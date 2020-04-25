abc4d9ee4a3ceef28f163312d16ec0601efdf776
sachinkum0009 <sachinkum123567@gmail.com>
2020-04-25 12:00:24 +0530

    service server and topic subscriber

--- turtle_pkg/scripts/grid_path_planning.py
+++ turtle_pkg/scripts/grid_path_planning.py
@@ -3,6 +3,7 @@
 import rospy
 from grid_based_sweep_path_planner import planning
 from sensor_msgs.msg import NavSatFix
+from turtle_pkg.msg import GpsCoordinates
 from move_base_msgs.msg import MoveBaseActionResult
 from std_srvs.srv import Empty
 
@@ -12,20 +13,32 @@ class GridPathPlannig:
     def __init__(self):
         rospy.init_node('grid_path_planning')
 
-        lat = 49.9000869191
-        lng = 8.89990548393
-        ox = [lat, lat+0.0000200, lat+0.0000200, lat, lat]
-        oy = [lng, lng, lng+0.0000200, lng+0.0000200, lng]
-        reso = 0.0000025
-
-        self.px, self.py = planning(ox, oy, reso)
+        self.lat = 49.9000869191
+        self.lng = 8.89990548393
+        self.sub = rospy.Subscriber('/lat_lng_sub', GpsCoordinates, self.feedback)
 
         self.i = 0
         rospy.wait_for_service('/move_base/clear_costmaps')
         self.clear_costmap = rospy.ServiceProxy('/move_base/clear_costmaps',Empty)
-        move_sub = rospy.Subscriber('/move_base/result',MoveBaseActionResult,self.feedback_cb)
+        self.move_sub = rospy.Subscriber('/move_base/result',MoveBaseActionResult,self.feedback_cb)
 
         rospy.spin()
+
+    def feedback(self, msg):
+        ox = [msg.lat1, msg.lat2, msg.lat3, msg.lat4, msg.lat1]
+        oy = [msg.lng1, msg.lng2, msg.lng3, msg.lng4, msg.lng1]
+
+        reso = 5.0
+        rospy.loginfo('got points')
+
+        self.px, self.py = planning(ox, oy, reso)
+        print(self.px)
+        print('--------')
+        print(self.py)
+
+        self.publish_previous_goal(self.i)
+
+
 
 
 
@@ -35,25 +48,25 @@ class GridPathPlannig:
         if (msg.status.status == 3):
             rospy.loginfo('result is success')
             rospy.logwarn('clearing costmap')
-            ret = self.clear_costmap()
+            #ret = self.clear_costmap()
             self.publish_next_goal(self.i)
         elif (msg.status.status == 2):
             rospy.loginfo('there is some error so clearing the costmap')
-            ret = self.clear_costmap()
+            #ret = self.clear_costmap()
             self.publish_previous_goal(self.i)
         elif (msg.status.status == 4):
             rospy.loginfo('there is some warning so clearing the costmap')
-            ret = self.clear_costmap()
+            #ret = self.clear_costmap()
             self.publish_previous_goal(self.i)
 
     def publish_previous_goal(self,i):
         gps_pub = rospy.Publisher('/gps_goal_fix', NavSatFix, queue_size=10)
         gps_data = NavSatFix()
         gps_data.header.frame_id='/world'
-        gps_data.latitude=self.px[self.i]
-        rospy.loginfo('latitude is : '+str(self.px[self.i]))
-        gps_data.longitude=self.py[self.i]
-        rospy.loginfo('longitude is : '+str(self.py[self.i]))
+        gps_data.latitude=self.lat+(0.0000001000*self.px[self.i])
+        rospy.loginfo('latitude is : '+str(self.lat+(0.0000001000*self.px[self.i])))
+        gps_data.longitude=self.lng+(0.0000001000*self.py[self.i])
+        rospy.loginfo('longitude is : '+str(self.lng+(0.0000001000*self.py[self.i])))
         rospy.logerr('number is '+str(self.i))
         rospy.loginfo('publishing next goal')
         gps_pub.publish(gps_data)
@@ -66,10 +79,10 @@ class GridPathPlannig:
         gps_data = NavSatFix()
         gps_data.header.frame_id='/world'
         rospy.logerr('number is '+str(self.i))
-        gps_data.latitude=self.px[self.i]
-        rospy.loginfo('latitude is : '+str(self.px[self.i]))
-        gps_data.longitude=self.py[self.i]
-        rospy.loginfo('longitude is : '+str(self.py[self.i]))
+        gps_data.latitude=self.lat+(0.0000001000*self.px[self.i])
+        rospy.loginfo('latitude is : '+str(self.lat+(0.0000001000*self.px[self.i])))
+        gps_data.longitude=self.lng+(0.0000001000*self.py[self.i])
+        rospy.loginfo('longitude is : '+str(self.lng+(0.0000001000*self.py[self.i])))
         rospy.loginfo('publishing next goal')
         gps_pub.publish(gps_data)
 
---
+++ turtle_pkg/scripts/start_goal_server.py
@@ -0,0 +1,25 @@
+import rospy
+from std_srvs.srv import Empty
+
+from move_base_msgs.msg import MoveBaseActionResult
+
+
+
+
+def main():
+    rospy.init_node('start_goal_server')
+    def start_move(request):
+        data = MoveBaseActionResult()
+        data.status.status = 3
+        pub.publish(data)
+        
+    server = rospy.Service('/start_goal_server', Empty, start_move)
+    pub = rospy.Publisher('/move_base/result', MoveBaseActionResult, queue_size=10)
+    
+    
+
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
