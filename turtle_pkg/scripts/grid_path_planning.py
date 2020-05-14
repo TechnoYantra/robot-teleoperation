6cc214622e109efb1ad8e9a71592b6cf64017409
sachinkum0009 <sachinkum123567@gmail.com>
2020-05-14 20:29:50 +0530

    added the grid_path_planner to launch file

--- turtle_pkg/scripts/grid_path_planning.py
+++ turtle_pkg/scripts/grid_path_planning.py
@@ -1,12 +1,581 @@
 #! /usr/bin/env python3
-
+import os
+print(os.system('python --version'))
 import rospy
-from grid_based_sweep_path_planner import planning
+# from grid_based_sweep_path_planner import planning
 from sensor_msgs.msg import NavSatFix
 from turtle_pkg.msg import GpsCoordinates
 from move_base_msgs.msg import MoveBaseActionResult
 from std_srvs.srv import Empty
 
+
+import math
+import os
+import sys
+from enum import IntEnum
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#sys.path.append(os.path.relpath("Mapping/grid_map_lib/"))
+#try:
+#    from grid_map_lib import GridMap
+#except ImportError:
+#    raise
+#
+# start here
+"""
+
+Grid map library in python
+
+author: Atsushi Sakai
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class GridMap:
+    """
+    GridMap class
+    """
+
+    def __init__(self, width, height, resolution,
+                 center_x, center_y, init_val=0.0):
+        """__init__
+
+        :param width: number of grid for width
+        :param height: number of grid for heigt
+        :param resolution: grid resolution [m]
+        :param center_x: center x position  [m]
+        :param center_y: center y position [m]
+        :param init_val: initial value for all grid
+        """
+        self.width = width
+        self.height = height
+        self.resolution = resolution
+        self.center_x = center_x
+        self.center_y = center_y
+
+        self.left_lower_x = self.center_x - \
+                            (self.width / 2.0) * self.resolution
+        self.left_lower_y = self.center_y - \
+                            (self.height / 2.0) * self.resolution
+
+        self.ndata = self.width * self.height
+        self.data = [init_val] * self.ndata
+
+    def get_value_from_xy_index(self, x_ind, y_ind):
+        """get_value_from_xy_index
+
+        when the index is out of grid map area, return None
+
+        :param x_ind: x index
+        :param y_ind: y index
+        """
+
+        grid_ind = self.calc_grid_index_from_xy_index(x_ind, y_ind)
+
+        if 0 <= grid_ind <= self.ndata:
+            return self.data[grid_ind]
+        else:
+            return None
+
+    def get_xy_index_from_xy_pos(self, x_pos, y_pos):
+        """get_xy_index_from_xy_pos
+
+        :param x_pos: x position [m]
+        :param y_pos: y position [m]
+        """
+        x_ind = self.calc_xy_index_from_position(
+            x_pos, self.left_lower_x, self.width)
+        y_ind = self.calc_xy_index_from_position(
+            y_pos, self.left_lower_y, self.height)
+
+        return x_ind, y_ind
+
+    def set_value_from_xy_pos(self, x_pos, y_pos, val):
+        """set_value_from_xy_pos
+
+        return bool flag, which means setting value is succeeded or not
+
+        :param x_pos: x position [m]
+        :param y_pos: y position [m]
+        :param val: grid value
+        """
+
+        x_ind, y_ind = self.get_xy_index_from_xy_pos(x_pos, y_pos)
+
+        if (not x_ind) or (not y_ind):
+            return False  # NG
+
+        flag = self.set_value_from_xy_index(x_ind, y_ind, val)
+
+        return flag
+
+    def set_value_from_xy_index(self, x_ind, y_ind, val):
+        """set_value_from_xy_index
+
+        return bool flag, which means setting value is succeeded or not
+
+        :param x_ind: x index
+        :param y_ind: y index
+        :param val: grid value
+        """
+
+        if (x_ind is None) or (y_ind is None):
+            print(x_ind, y_ind)
+            return False, False
+
+        grid_ind = int(y_ind * self.width + x_ind)
+
+        if 0 <= grid_ind < self.ndata:
+            self.data[grid_ind] = val
+            return True  # OK
+        else:
+            return False  # NG
+
+    def set_value_from_polygon(self, pol_x, pol_y, val, inside=True):
+        """set_value_from_polygon
+
+        Setting value inside or outside polygon
+
+        :param pol_x: x position list for a polygon
+        :param pol_y: y position list for a polygon
+        :param val: grid value
+        :param inside: setting data inside or outside
+        """
+
+        # making ring polygon
+        if (pol_x[0] != pol_x[-1]) or (pol_y[0] != pol_y[-1]):
+            pol_x.append(pol_x[0])
+            pol_y.append(pol_y[0])
+
+        # setting value for all grid
+        for x_ind in range(self.width):
+            for y_ind in range(self.height):
+                x_pos, y_pos = self.calc_grid_central_xy_position_from_xy_index(
+                    x_ind, y_ind)
+
+                flag = self.check_inside_polygon(x_pos, y_pos, pol_x, pol_y)
+
+                if flag is inside:
+                    self.set_value_from_xy_index(x_ind, y_ind, val)
+
+    def calc_grid_index_from_xy_index(self, x_ind, y_ind):
+        grid_ind = int(y_ind * self.width + x_ind)
+        return grid_ind
+
+    def calc_grid_central_xy_position_from_xy_index(self, x_ind, y_ind):
+        x_pos = self.calc_grid_central_xy_position_from_index(
+            x_ind, self.left_lower_x)
+        y_pos = self.calc_grid_central_xy_position_from_index(
+            y_ind, self.left_lower_y)
+
+        return x_pos, y_pos
+
+    def calc_grid_central_xy_position_from_index(self, index, lower_pos):
+        return lower_pos + index * self.resolution + self.resolution / 2.0
+
+    def calc_xy_index_from_position(self, pos, lower_pos, max_index):
+        ind = int(np.floor((pos - lower_pos) / self.resolution))
+        if 0 <= ind <= max_index:
+            return ind
+        else:
+            return None
+
+    def check_occupied_from_xy_index(self, xind, yind, occupied_val=1.0):
+
+        val = self.get_value_from_xy_index(xind, yind)
+
+        if val >= occupied_val:
+            return True
+        else:
+            return False
+
+    def expand_grid(self):
+        xinds, yinds = [], []
+
+        for ix in range(self.width):
+            for iy in range(self.height):
+                if self.check_occupied_from_xy_index(ix, iy):
+                    xinds.append(ix)
+                    yinds.append(iy)
+
+        for (ix, iy) in zip(xinds, yinds):
+            self.set_value_from_xy_index(ix + 1, iy, val=1.0)
+            self.set_value_from_xy_index(ix, iy + 1, val=1.0)
+            self.set_value_from_xy_index(ix + 1, iy + 1, val=1.0)
+            self.set_value_from_xy_index(ix - 1, iy, val=1.0)
+            self.set_value_from_xy_index(ix, iy - 1, val=1.0)
+            self.set_value_from_xy_index(ix - 1, iy - 1, val=1.0)
+
+    @staticmethod
+    def check_inside_polygon(iox, ioy, x, y):
+
+        npoint = len(x) - 1
+        inside = False
+        for i1 in range(npoint):
+            i2 = (i1 + 1) % (npoint + 1)
+
+            if x[i1] >= x[i2]:
+                min_x, max_x = x[i2], x[i1]
+            else:
+                min_x, max_x = x[i1], x[i2]
+            if not min_x < iox < max_x:
+                continue
+
+            if (y[i1] + (y[i2] - y[i1]) / (x[i2] - x[i1])
+                * (iox - x[i1]) - ioy) > 0.0:
+                inside = not inside
+
+        return inside
+
+    def plot_grid_map(self, ax=None):
+
+        grid_data = np.reshape(np.array(self.data), (self.height, self.width))
+        if not ax:
+            fig, ax = plt.subplots()
+        heat_map = ax.pcolor(grid_data, cmap="Blues", vmin=0.0, vmax=1.0)
+        plt.axis("equal")
+        # plt.show()
+
+        return heat_map
+
+
+def test_polygon_set():
+    ox = [0.0, 20.0, 50.0, 100.0, 130.0, 40.0]
+    oy = [0.0, -20.0, 0.0, 30.0, 60.0, 80.0]
+
+    grid_map = GridMap(600, 290, 0.7, 60.0, 30.5)
+
+    grid_map.set_value_from_polygon(ox, oy, 1.0, inside=False)
+
+    grid_map.plot_grid_map()
+
+    plt.axis("equal")
+    plt.grid(True)
+
+
+def test_position_set():
+    grid_map = GridMap(100, 120, 0.5, 10.0, -0.5)
+
+    grid_map.set_value_from_xy_pos(10.1, -1.1, 1.0)
+    grid_map.set_value_from_xy_pos(10.1, -0.1, 1.0)
+    grid_map.set_value_from_xy_pos(10.1, 1.1, 1.0)
+    grid_map.set_value_from_xy_pos(11.1, 0.1, 1.0)
+    grid_map.set_value_from_xy_pos(10.1, 0.1, 1.0)
+    grid_map.set_value_from_xy_pos(9.1, 0.1, 1.0)
+
+    grid_map.plot_grid_map()
+
+
+
+# end here
+do_animation = True
+
+
+class SweepSearcher:
+    class SweepDirection(IntEnum):
+        UP = 1
+        DOWN = -1
+
+    class MovingDirection(IntEnum):
+        RIGHT = 1
+        LEFT = -1
+
+    def __init__(self, mdirection, sdirection, xinds_goaly, goaly):
+        self.moving_direction = mdirection
+        self.sweep_direction = sdirection
+        self.turing_window = []
+        self.update_turning_window()
+        self.xinds_goaly = xinds_goaly
+        self.goaly = goaly
+
+    def move_target_grid(self, cxind, cyind, gmap):
+        nxind = self.moving_direction + cxind
+        nyind = cyind
+
+        # found safe grid
+        if not gmap.check_occupied_from_xy_index(nxind, nyind, occupied_val=0.5):
+            return nxind, nyind
+        else:  # occupided
+            ncxind, ncyind = self.find_safe_turning_grid(cxind, cyind, gmap)
+            if (ncxind is None) and (ncyind is None):
+                # moving backward
+                ncxind = -self.moving_direction + cxind
+                ncyind = cyind
+                if gmap.check_occupied_from_xy_index(ncxind, ncyind):
+                    # moved backward, but the grid is occupied by obstacle
+                    return None, None
+            else:
+                # keep moving until end
+                while not gmap.check_occupied_from_xy_index(ncxind + self.moving_direction, ncyind, occupied_val=0.5):
+                    ncxind += self.moving_direction
+                self.swap_moving_direction()
+            return ncxind, ncyind
+
+    def find_safe_turning_grid(self, cxind, cyind, gmap):
+
+        for (dxind, dyind) in self.turing_window:
+
+            nxind = dxind + cxind
+            nyind = dyind + cyind
+
+            # found safe grid
+            if not gmap.check_occupied_from_xy_index(nxind, nyind, occupied_val=0.5):
+                return nxind, nyind
+
+        return None, None
+
+    def is_search_done(self, gmap):
+        for ix in self.xinds_goaly:
+            if not gmap.check_occupied_from_xy_index(ix, self.goaly, occupied_val=0.5):
+                return False
+
+        # all lower grid is occupied
+        return True
+
+    def update_turning_window(self):
+        self.turing_window = [
+            (self.moving_direction, 0.0),
+            (self.moving_direction, self.sweep_direction),
+            (0, self.sweep_direction),
+            (-self.moving_direction, self.sweep_direction),
+        ]
+
+    def swap_moving_direction(self):
+        self.moving_direction *= -1
+        self.update_turning_window()
+
+    def search_start_grid(self, grid_map):
+        xinds = []
+        y_ind = 0
+        if self.sweep_direction == self.SweepDirection.DOWN:
+            xinds, y_ind = search_free_grid_index_at_edge_y(grid_map, from_upper=True)
+        elif self.sweep_direction == self.SweepDirection.UP:
+            xinds, y_ind = search_free_grid_index_at_edge_y(grid_map, from_upper=False)
+
+        if self.moving_direction == self.MovingDirection.RIGHT:
+            return min(xinds), y_ind
+        elif self.moving_direction == self.MovingDirection.LEFT:
+            return max(xinds), y_ind
+
+        raise ValueError("self.moving direction is invalid ")
+
+
+def find_sweep_direction_and_start_posi(ox, oy):
+    # find sweep_direction
+    max_dist = 0.0
+    vec = [0.0, 0.0]
+    sweep_start_pos = [0.0, 0.0]
+    for i in range(len(ox) - 1):
+        dx = ox[i + 1] - ox[i]
+        dy = oy[i + 1] - oy[i]
+        d = np.hypot(dx, dy)
+
+        if d > max_dist:
+            max_dist = d
+            vec = [dx, dy]
+            sweep_start_pos = [ox[i], oy[i]]
+
+    return vec, sweep_start_pos
+
+
+def convert_grid_coordinate(ox, oy, sweep_vec, sweep_start_posi):
+    tx = [ix - sweep_start_posi[0] for ix in ox]
+    ty = [iy - sweep_start_posi[1] for iy in oy]
+
+    th = math.atan2(sweep_vec[1], sweep_vec[0])
+
+    c = np.cos(-th)
+    s = np.sin(-th)
+
+    rx = [ix * c - iy * s for (ix, iy) in zip(tx, ty)]
+    ry = [ix * s + iy * c for (ix, iy) in zip(tx, ty)]
+
+    return rx, ry
+
+
+def convert_global_coordinate(x, y, sweep_vec, sweep_start_posi):
+    th = math.atan2(sweep_vec[1], sweep_vec[0])
+    c = np.cos(th)
+    s = np.sin(th)
+
+    tx = [ix * c - iy * s for (ix, iy) in zip(x, y)]
+    ty = [ix * s + iy * c for (ix, iy) in zip(x, y)]
+
+    rx = [ix + sweep_start_posi[0] for ix in tx]
+    ry = [iy + sweep_start_posi[1] for iy in ty]
+
+    return rx, ry
+
+
+def search_free_grid_index_at_edge_y(grid_map, from_upper=False):
+    yind = None
+    xinds = []
+
+    if from_upper:
+        xrange = range(grid_map.height)[::-1]
+        yrange = range(grid_map.width)[::-1]
+    else:
+        xrange = range(grid_map.height)
+        yrange = range(grid_map.width)
+
+    for iy in xrange:
+        for ix in yrange:
+            if not grid_map.check_occupied_from_xy_index(ix, iy):
+                yind = iy
+                xinds.append(ix)
+        if yind:
+            break
+
+    return xinds, yind
+
+
+def setup_grid_map(ox, oy, reso, sweep_direction, offset_grid=10):
+    width = math.ceil((max(ox) - min(ox)) / reso) + offset_grid
+    height = math.ceil((max(oy) - min(oy)) / reso) + offset_grid
+    center_x = np.mean(ox)
+    center_y = np.mean(oy)
+
+    grid_map = GridMap(width, height, reso, center_x, center_y)
+
+    grid_map.set_value_from_polygon(ox, oy, 1.0, inside=False)
+
+    grid_map.expand_grid()
+
+    xinds_goaly = []
+    goaly = 0
+    if sweep_direction == SweepSearcher.SweepDirection.UP:
+        xinds_goaly, goaly = search_free_grid_index_at_edge_y(grid_map, from_upper=True)
+    elif sweep_direction == SweepSearcher.SweepDirection.DOWN:
+        xinds_goaly, goaly = search_free_grid_index_at_edge_y(grid_map, from_upper=False)
+
+    return grid_map, xinds_goaly, goaly
+
+
+def sweep_path_search(sweep_searcher, gmap, grid_search_animation=False):
+    # search start grid
+    cxind, cyind = sweep_searcher.search_start_grid(gmap)
+    if not gmap.set_value_from_xy_index(cxind, cyind, 0.5):
+        print("Cannot find start grid")
+        return [], []
+
+    x, y = gmap.calc_grid_central_xy_position_from_xy_index(cxind, cyind)
+    px, py = [x], [y]
+
+    if grid_search_animation:
+        fig, ax = plt.subplots()
+        # for stopping simulation with the esc key.
+        fig.canvas.mpl_connect('key_release_event',
+                lambda event: [exit(0) if event.key == 'escape' else None])
+
+    while True:
+        cxind, cyind = sweep_searcher.move_target_grid(cxind, cyind, gmap)
+
+        if sweep_searcher.is_search_done(gmap) or (cxind is None or cyind is None):
+            print("Done")
+            break
+
+        x, y = gmap.calc_grid_central_xy_position_from_xy_index(
+            cxind, cyind)
+
+        px.append(x)
+        py.append(y)
+
+        gmap.set_value_from_xy_index(cxind, cyind, 0.5)
+
+        if grid_search_animation:
+            gmap.plot_grid_map(ax=ax)
+            plt.pause(1.0)
+
+    gmap.plot_grid_map()
+
+    return px, py
+
+
+def planning(ox, oy, reso,
+             moving_direction=SweepSearcher.MovingDirection.RIGHT,
+             sweeping_direction=SweepSearcher.SweepDirection.UP,
+             ):
+    sweep_vec, sweep_start_posi = find_sweep_direction_and_start_posi(ox, oy)
+
+    rox, roy = convert_grid_coordinate(ox, oy, sweep_vec, sweep_start_posi)
+
+    gmap, xinds_goaly, goaly = setup_grid_map(rox, roy, reso, sweeping_direction)
+
+    sweep_searcher = SweepSearcher(moving_direction, sweeping_direction, xinds_goaly, goaly)
+
+    px, py = sweep_path_search(sweep_searcher, gmap)
+
+    rx, ry = convert_global_coordinate(px, py, sweep_vec, sweep_start_posi)
+
+    print("Path length:", len(rx))
+
+    return rx, ry
+
+
+def planning_animation(ox, oy, reso):  # pragma: no cover
+    px, py = planning(ox, oy, reso)
+    print(px)
+    print('---------')
+    print(py)
+
+    # animation
+    if do_animation:
+        for ipx, ipy in zip(px, py):
+            plt.cla()
+            # for stopping simulation with the esc key.
+            plt.gcf().canvas.mpl_connect('key_release_event',
+                    lambda event: [exit(0) if event.key == 'escape' else None])
+            plt.plot(ox, oy, "-xb")
+            plt.plot(px, py, "-r")
+            plt.plot(ipx, ipy, "or")
+            plt.axis("equal")
+            plt.grid(True)
+            plt.pause(0.1)
+
+    plt.cla()
+    plt.plot(ox, oy, "-xb")
+    plt.plot(px, py, "-r")
+    plt.axis("equal")
+    plt.grid(True)
+    plt.pause(0.1)
+
+
+def main():  # pragma: no cover
+    print("start!!")
+    '''
+    ox = [0.0, 20.0, 50.0, 100.0, 130.0, 40.0, 0.0]
+    oy = [0.0, -20.0, 0.0, 30.0, 60.0, 80.0, 0.0]
+    reso = 5.0
+    planning_animation(ox, oy, reso)
+    
+    # latitude = 49.9000869191
+    # longitude = 8.89990548393
+    lat = 49.9000869191
+    lng = 8.89990548393
+    ox = [lat, lat+0.0000200, lat+0.0000200, lat, lat]
+    oy = [lng, lng, lng+0.0000200, lng+0.0000200, lng]
+    reso = 0.0000025
+    planning_animation(ox, oy, reso)
+    
+    ox = [0.0, 20.0, 50.0, 200.0, 130.0, 40.0, 0.0]
+    oy = [0.0, -80.0, 0.0, 30.0, 60.0, 80.0, 0.0]
+    reso = 5.0
+    planning_animation(ox, oy, reso)
+    '''
+
+    ox=  [-10.856999397277832, -9.560998916625977, 9.447001457214355, 7.395001411437988, -10.856999397277832]
+    oy = [8.912402153015137, -9.339598655700684, -6.747598648071289, 12.476402282714844, 8.912402153015137]
+
+    reso = 3.0
+    planning_animation(ox, oy, reso)
+    plt.show()
+
+    print("done!!")
 
 
 class GridPathPlannig:
